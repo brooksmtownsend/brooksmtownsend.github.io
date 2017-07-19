84cd1ac89a1cc61b32549b28baf4b74e137f6d4a
Brooks Townsend <brooksmtownsend@gmail.com>
2017-07-19 07:57:24 -0400

    Parity with brooksmtownsend.github.io

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 function toggleToolbar(){
-    var toolbar = document.getElementById("toolbar");
-    var button = document.getElementById("toolbarButton");
+    var toolbar = document.getElementById("hamburger-menu");
     if (toolbar.style.display == "flex"){
         toolbar.style.display = "none";
     } else{
         toolbar.style.display = "flex";
-        toolbar.style.marginTop = "2.5vw";
+        toolbar.style.marginTop = "2vw";
     }
 }
