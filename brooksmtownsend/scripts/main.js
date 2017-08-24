9a633d72380f4f78aa12d7a68066d4e3b7a2e912
Brooks Townsend <brooksmtownsend@gmail.com>
2017-08-24 16:16:18 -0400

    deleted old website and duplicate folder

--- brooksmtownsend/scripts/main.js
+++
@@ -1,9 +0,0 @@
-function toggleToolbar(){
-    var toolbar = document.getElementById("hamburger-menu");
-    if (toolbar.style.display == "flex"){
-        toolbar.style.display = "none";
-    } else{
-        toolbar.style.display = "flex";
-        toolbar.style.marginTop = "2vw";
-    }
-}
