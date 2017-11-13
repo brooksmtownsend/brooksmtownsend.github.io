e2bf2cbf0131e6713336c3018cfc07d720b11bd0
Brooks Townsend <brooksmtownsend@gmail.com>
2017-11-13 12:34:22 -0500

    Updated build Mon Nov 13 12:34:22 EST 2017

--- updateBuild.sh
+++ updateBuild.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo "Copying build files...."
 cp * -r ../metabuilding/websites/brooksmtownsend-react/build/* ./
+echo "Gitting and updating"
+
 git status
 git add --all
 git commit -m "Updated build $(date)"
 git pull origin master
 git push origin master
-echo("")
-echo("You updated your build, woo")
+
+echo "You updated your build, woo"
