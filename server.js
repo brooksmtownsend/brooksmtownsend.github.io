e9aca46df2edb3b058735dbf88df5998b3016dc7
Ubuntu <ubuntu@ip-172-31-13-84.us-west-2.compute.internal>
2018-04-12 23:49:23 +0000

    updated server.js to reflect current directory, new icon

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express()
 const path = require('path')
 const port = 8080
 
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/build/'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join('./build/index.html'))
+    res.sendFile(path.join(__dirname, 'build/', 'index.html'))
 })
 
 app.listen(port, '172.31.13.84', (err) => {
